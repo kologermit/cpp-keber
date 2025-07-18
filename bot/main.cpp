98ad2695fe932f1faa4f4032e52a7cf3a0f5c33a
kologermit@kologermit.ru <kologermit@gmail.com>
2025-07-18 12:30:16 +0000

    Обновил bot/main.cpp

--- bot/main.cpp
+++ bot/main.cpp
@@ -1,42 +1,68 @@
-#include <memory>
-#include <iostream>
-#include <string>
-#include <utils/Types.hpp>
-#include <utils/Env/Env.hpp>
-#include <utils/TGBotApi/Bot/InterfaceBot.hpp>
-#include <utils/TGBotApi/Bot/Bot.hpp>
-#include <utils/TGBotApi/Query.hpp>
-#include <utils/TGBotApi/JSONKeys.hpp>
-#include <utils/Logger/InterfaceLogger.hpp>
 #include <utils/Logger/StdOutLogger.hpp>
+#include <utils/Env/Env.hpp>
+#include <utils/TGBotApi/Bot/Bot.hpp>
+#include <utils/TGBotApi/Message/Keyboard/ReplyKeyboard.hpp>
+#include <utils/TGBotApi/Message/Keyboard/ReplyButton.hpp>
+#include <utils/TGBotApi/Message/Keyboard/InlineKeyboard.hpp>
+#include <utils/TGBotApi/Message/Keyboard/InlineButton.hpp>
 
-using Utils::Logger::GLOBAL_LOGGER;
+using Utils::TGBotApi::File::EnumContentType;
+using Utils::Logger::get_logger;
 using Utils::Logger::StdOutLogger;
-using Utils::Types::const_string;
-using Utils::TGBotApi::Bot::InterfaceBot;
 using Utils::TGBotApi::Bot::Bot;
-using std::make_shared, std::shared_ptr;
-using std::string, std::cout, std::cin, std::to_string;
+using Utils::TGBotApi::Message::Keyboard::ReplyKeyboard;
+using Utils::TGBotApi::Message::Keyboard::ReplyButton;
+using Utils::TGBotApi::Message::Keyboard::InlineKeyboard;
+using Utils::TGBotApi::Message::Keyboard::InlineButton;
+using Utils::TGBotApi::Bot::ptrBot;
+using std::to_string;
+using std::make_shared;
+using std::string;
+using std::nullopt;
+const int MESSAGE_ID = 2723;
+const int USER_ID = 847721936;
+string TOKEN = Utils::Env::Get("BOT_TOKEN");
+string ANSWER_TEXT = "<code>answer</code>";
+string PHOTO_FILEPATH = "/home/kologermit/audio.mp3";
 
 int main() {
-    GLOBAL_LOGGER = make_shared<StdOutLogger>();
-    const_string token = Utils::Env::Get("BOT_TOKEN");
-    GLOBAL_LOGGER->info("TOKEN", token);
-    shared_ptr<InterfaceBot> bot = make_shared<Bot>(token);
-    GLOBAL_LOGGER->info("BOT NAME", bot->get_name());
-    GLOBAL_LOGGER->info("BOT USERNAME", bot->get_username());
-    string message_text; cout << "Input text: "; cin >> message_text;
-    const int user_id = 847721936;
-    GLOBAL_LOGGER->info("TEXT FROM INPUT", message_text);
-    GLOBAL_LOGGER->info("USER ID", to_string(user_id));
-    auto message = bot->send_text(user_id, message_text);
-    auto chat = message->chat();
-    auto from = message->from();
-    GLOBAL_LOGGER->info("(q) MESSAGE TEXT", message->get_text());
-    GLOBAL_LOGGER->info("(q) MESSAGE ID", to_string(message->get_id()));
-    GLOBAL_LOGGER->info("(q) CHAT ID", to_string(chat->get_id()));
-    GLOBAL_LOGGER->info("(q) CHAT NAME", chat->get_name());
-    GLOBAL_LOGGER->info("(q) FROM ID", to_string(from->get_id()));
-    GLOBAL_LOGGER->info("(q) FROM NAME", chat->get_name());
-    
+    auto logger = get_logger(new StdOutLogger);
+    ptrBot bot = make_shared<Bot>(TOKEN);
+
+    bot->send_message({
+        .chat_id = USER_ID,
+        .text = ANSWER_TEXT,
+        .reply_message_id = MESSAGE_ID,
+        .filepath = PHOTO_FILEPATH,
+        .content_type = EnumContentType::AUDIO,
+        .inline_keyboard = make_shared<InlineKeyboard>(
+            InlineKeyboard::ptrButtons(new InlineKeyboard::Buttons{
+                { make_shared<InlineButton>("Проверка callback", nullopt, "123") },
+                { make_shared<InlineButton>("Ссылка на VK", "https://vk.com/kologermit"), make_shared<InlineButton>("Ссылка на ТГ", "https://t.me/kologermit.ru") }
+            })
+        )
+    });
+
+    auto message = bot->send_message({
+        .chat_id = USER_ID,
+        .text = ANSWER_TEXT,
+        .reply_message_id = MESSAGE_ID,
+        .filepath = PHOTO_FILEPATH,
+        .content_type = EnumContentType::AUDIO,
+        .reply_keyboard = make_shared<ReplyKeyboard>(
+            ReplyKeyboard::ptrButtons(new ReplyKeyboard::Buttons{
+                { make_shared<ReplyButton>("Проверка callback") },
+                { make_shared<ReplyButton>("Ссылка на VK"), make_shared<ReplyButton>("Ссылка на ТГ") }
+            })
+        )
+    });
+
+    logger->info("MESSAGE_TEXT", *message->get_text());
+    logger->info("MESSAGE_DOWNLOAD_ID", *message->get_file_download_id());
+    logger->info("MESSAGE_CONTENT_TYPE", to_string(message->get_file_content_type()));
+    logger->info("MESSAGE_DOWNLOAD_ID", *message->get_file_name());
+    logger->info("MESSAGE_FILE_SIZE (kb)", to_string(*message->get_file_size() / 1024));
+
+    logger->info("REPLY_MESSAGE_ID", to_string(message->get_reply_message()->get_id()));
+    logger->info("REPLY_MESSAGE_TEXT", *message->get_reply_message()->get_text());
 }
