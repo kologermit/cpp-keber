0036a7ded8b98ed6c42cdb58a10ab90d17b59e8e
kologermit <kologermit@gmail.com>
2026-02-23 20:12:15 +0300

    Добавил сортировку по названию в ежедневик

--- bot/BotHandler/TaskTracker/TaskTrackerHandler.cpp
+++ bot/BotHandler/TaskTracker/TaskTrackerHandler.cpp
@@ -15,6 +15,7 @@ namespace Bot::BotHandler::TaskTracker {
     using std::max;
     using std::to_string;
     using std::optional;
+    using std::ranges::sort;
     using nlohmann::json;
     using jed_utils::datetime;
     using Utils::TGBotApi::Types::ptrMessage;
@@ -34,6 +35,10 @@ namespace Bot::BotHandler::TaskTracker {
     using Bot::Entity::User::UserScreen;
     using Bot::BotHandler::Menu::MenuHandler;
     using Bot::BotHandler::TaskTracker::Description::DescriptionHandler;
+
+    bool task_title_cmp(const Task& a, const Task& b) {
+        return a.title < b.title;
+    };
 
     const string& TaskTrackerHandler::get_name() const noexcept {
         static const string name = "TaskTracker";
@@ -155,23 +160,27 @@ namespace Bot::BotHandler::TaskTracker {
             .start_at_lte = today_end,
             .state = TaskState::NEW,
         });
+        sort(*new_tasks, task_title_cmp);
         const unique_ptr<vector<Task> > in_work_tasks = ctx->global_ctx->api->task_tracker->get_tasks({
             .user_id = ctx->user->id,
             .start_at_lte = today_end,
             .state = TaskState::IN_WORK,
         });
+        sort(*in_work_tasks, task_title_cmp);
         const unique_ptr<vector<Task> > completed_tasks = ctx->global_ctx->api->task_tracker->get_tasks({
             .user_id = ctx->user->id,
             .completed_at_gte = today_start,
             .completed_at_lte = today_end,
             .state = TaskState::COMPLETED,
         });
+        sort(*completed_tasks, task_title_cmp);
         const unique_ptr<vector<Task> > deleted_tasks = ctx->global_ctx->api->task_tracker->get_tasks({
             .user_id = ctx->user->id,
             .deleted_at_gte = today_start,
             .deleted_at_lte = today_end,
             .state = TaskState::DELETED,
         });
+        sort(*deleted_tasks, task_title_cmp);
         string message_text = fmt::format("<b>Статистика за сегодня ({})</b>", today.to_string(DATE_FORMAT));
         message_text += fmt::format("\n\n<b>Новые ({}):</b>", new_tasks->size());
         for (const Task& task : *new_tasks) {
@@ -230,12 +239,14 @@ namespace Bot::BotHandler::TaskTracker {
             .start_at_lte = start_at,
             .state = TaskState::NEW,
         });
+        sort(*new_tasks, task_title_cmp);
         const unique_ptr<vector<Task>> in_work_tasks = ctx->global_ctx->api->task_tracker->get_tasks({
             .user_id = ctx->user->id,
             .start_at_gte = start_at_gte,
             .start_at_lte = start_at,
             .state = TaskState::IN_WORK,
         });
+        sort(*in_work_tasks, task_title_cmp);
         vector<Task> tasks(*in_work_tasks);
         tasks.insert(tasks.end(), new_tasks->begin(), new_tasks->end());
         string result_text;
